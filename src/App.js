2b5dd22d22d0a10234c6448dfcd74e2a08d9a2da
AaronMcGloin1506 <amcgloin94@gmail.com>
2021-09-20 18:27:47 +0100

    Pure compnents

--- src/App.js
+++ src/App.js
@@ -3,14 +3,21 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom'
 
 import Home from './components/home';
 import Header from './components/header';
+import PureComp from './components/pureComp'
+import PureCompF from './components/pureCompf'
 
 const App = () => {
   return (
     <BrowserRouter>
-      <Header/>
-      <Switch>
+       <Header/>
+       <div className="container">
+        <Switch>
+        <Route path="/purecomp" component={PureComp} />
+        <Route path="/purecompf" component={PureCompF} />
         <Route path="/" component={Home} />
-      </Switch>
+        </Switch>
+       </div>
+      
     </BrowserRouter>
   );
 }
